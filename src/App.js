fe00eb867de2fba83856af31bde13199b9b52292
Kylist <quocanhhnams1316@gmail.com>
2020-09-24 21:51:29 -0400

    Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import Tag from './components/Tag_V1';
+
+import './App.css';
+
+const App = () => (
+    <div>
+        <h1>Random GIF Application</h1>
+        <Tag />
+        <div className="main-container">
+           
+            
+        </div>
+    </div>
+);
+
+export default App;
